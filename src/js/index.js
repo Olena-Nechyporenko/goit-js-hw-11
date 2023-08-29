641bd79cca2dabbcd7ccba62f20464e517b4f0a7
Olena-Nechyporenko <olenan0309@gmail.com>
2023-08-29 11:16:53 +0300

    commit

---
+++ src/js/index.js
@@ -0,0 +1,62 @@
+import axios from "axios"
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_C8fch3W7tmmF8fmpsXU2mdIfjeqC3R8Q3wAY9gs2QZiQFnTO5tNKg722MPHavC8b';
+
+BASE_URL = "https://pixabay.com/api/"
+
+const elem = {
+    form: document.querySelector(".search-form"),
+    input: document.querySelector(".input-form"),
+    searchBtn: document.querySelector(".search-btn")
+}
+
+elem.form.addEventListener("submit", onSubmit)
+
+function onSubmit(evt) {
+    evt.preventDefault()
+    console.log(evt.target)
+}
+
+
+
+
+
+
+// function searchImages() {
+//     return axios
+//     .get(`${BASE_URL}?key=39125573-a5e5a696ee6b560e318b28a02&image_type=photo&orientation=horizontal&safesearch=true`)
+//     .then(response => {
+//       return response;
+//     })
+//     .catch(error => {
+//       throw new Error('Помилка запиту:', error.message);
+//     });
+// }
+
+// console.log(searchImages())
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // async function getUser() {
+//     try {
+//       const response = await axios.get('/user?ID=12345');
+//       console.log(response);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+// function getUserAccount() {
+//     return axios.get('/user/12345');
+//   }
