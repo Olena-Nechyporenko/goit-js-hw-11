340165bf221089f6797fdba34f6f6ca7402df6b1
Olena-Nechyporenko <olenan0309@gmail.com>
2023-08-31 17:36:14 +0300

    finished task

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const BASE_URL = "https://pixabay.com/api/";
 
@@ -12,6 +13,7 @@ const elem = {
     loadMoreBtn:document.querySelector(".load-more"),
     guard: document.querySelector(".js-guard")
 }
+
 let page = 1;
 
 elem.form.addEventListener("submit", onSubmit)
@@ -22,17 +24,22 @@ function onSubmit(evt) {
     .then(data => {
         page = 1;
         elem.gallery.innerHTML = renderMarkupInfo(data);
-        Notify.success(`Hooray! We found ${data.totalHits} images!`); 
+        Notify.success(`Hooray! We found ${data.totalHits} images!`, {width: "350px", fontSize: "20px"}); 
+        new SimpleLightbox('.gallery a')
+       
        const pages = Math.ceil(data.totalHits/data.hits.length);
        if(page < pages) {
         elem.loadMoreBtn.classList.replace("is-hidden", "load-more");
        }
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      elem.gallery.innerHTML = '';
+      elem.loadMoreBtn.classList.replace("load-more", "is-hidden");
+      console.log("Error :", err.message)});
 }
 
 
-  async function searchImages(currentPage = "1") {
+  async function searchImages(currentPage) {
     const params = new URLSearchParams({
         key: "39125573-a5e5a696ee6b560e318b28a02",
         q: elem.input.value,
@@ -44,9 +51,8 @@ function onSubmit(evt) {
     })
         const response = await axios.get(`${BASE_URL}?${params}`)
         if(response.data.hits.length === 0) {
-            Notify.warning("Sorry, there are no images matching your search query. Please try again.", {position: "center-top",distance: '100px'}); 
+           return Notify.warning("Sorry, there are no images matching your search query. Please try again.", {width: "400px"}); 
         }
-        
       return response.data;
   }
 
@@ -57,7 +63,7 @@ function onSubmit(evt) {
         numbers : "not found"
     }
         return data.hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) =>
-        ` <div class="photo-card">
+        ` <a class="photo-card" href="${largeImageURL}">
         <img src="${webformatURL || defaults.image}" alt="${tags}" width = "300" height = "200" loading="lazy" />
         <div class="info">
           <p class="info-item">
@@ -77,7 +83,7 @@ function onSubmit(evt) {
             <span class = "info-item-span">${downloads || defaults.numbers}</span>
           </p>
         </div>
-      </div>`).join('')
+      </a>`).join('')
 }
 
 
@@ -88,23 +94,23 @@ function handlerLoadMore() {
   page += 1;
   searchImages(page)
     .then((data) => {
-      elem.gallery.insertAdjacentHTML(
-        "beforeend",
-        renderMarkupInfo(data)
-      );
+      elem.gallery.insertAdjacentHTML("beforeend", renderMarkupInfo(data));
+       new SimpleLightbox('.gallery a')
+       
       const pages = Math.ceil(data.totalHits/data.hits.length)
       if(page < pages) {
         elem.loadMoreBtn.classList.replace("is-hidden", "load-more")
        }
-      if (page >= pages) {
+      if(page >= pages) {
         elem.loadMoreBtn.classList.replace("load-more", "is-hidden");
-        Notify.warning("We're sorry, but you've reached the end of search results.", {position: "center-bottom"}); 
-      }
-    })
+          return Notify.warning("We're sorry, but you've reached the end of search results.", {position: "center-bottom", fontSize: "25px", width: "450px", position: "center-bottom"});
+         } 
+      })
     .catch((err) => {
       elem.loadMoreBtn.classList.replace("load-more", "is-hidden");
     });
-   console.log(page)
+  
+ 
 }
 
 
