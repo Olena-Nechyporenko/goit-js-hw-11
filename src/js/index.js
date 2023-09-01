9710276fd9f79cb52c86825e3f213fcb4647d1e9
Olena-Nechyporenko <olenan0309@gmail.com>
2023-09-01 23:29:11 +0300

    fixed errors

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,9 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+import { renderMarkupInfo } from "./markup";
+
+
 const BASE_URL = "https://pixabay.com/api/";
 
 const elem = {
@@ -16,101 +19,66 @@ const elem = {
 
 let page = 1;
 
+const lightbox = new SimpleLightbox('.gallery a')
+
 elem.form.addEventListener("submit", onSubmit)
 
-function onSubmit(evt) {
+async function onSubmit(evt) {
     evt.preventDefault();
-    searchImages() 
-    .then(data => {
-        page = 1;
-        elem.gallery.innerHTML = renderMarkupInfo(data);
-        Notify.success(`Hooray! We found ${data.totalHits} images!`, {width: "350px", fontSize: "20px"}); 
-        new SimpleLightbox('.gallery a')
-       
-       const pages = Math.ceil(data.totalHits/data.hits.length);
+    const response = await searchImages() 
+    try {
+        if(response.hits.length === 0 || elem.input.value.trim() === "") {
+         return Notify.warning("Sorry, there are no images matching your search query. Please try again.", {width: "400px"}); 
+      }
+        
+        elem.gallery.innerHTML = renderMarkupInfo(response);
+        lightbox.refresh()
+        Notify.success(`Hooray! We found ${response.totalHits} images!`, {width: "350px", fontSize: "20px"}); 
+
+       const pages = Math.ceil(response.totalHits/40);
        if(page < pages) {
         elem.loadMoreBtn.classList.replace("is-hidden", "load-more");
        }
-    })
-    .catch(err => {
+    } catch(err) {
       elem.gallery.innerHTML = '';
       elem.loadMoreBtn.classList.replace("load-more", "is-hidden");
-      console.log("Error :", err.message)});
+      console.log("Error :", err.message)};
 }
 
-
-  async function searchImages(currentPage) {
-    const params = new URLSearchParams({
-        key: "39125573-a5e5a696ee6b560e318b28a02",
-        q: elem.input.value,
-        image_type: "photo",
-        orientation: "horizontal",
-        safesearch: true,
-        per_page: 40,
-        page: currentPage
-    })
-        const response = await axios.get(`${BASE_URL}?${params}`)
-        if(response.data.hits.length === 0) {
-           return Notify.warning("Sorry, there are no images matching your search query. Please try again.", {width: "400px"}); 
-        }
-      return response.data;
-  }
-
-
-  function renderMarkupInfo(data) {
-    const defaults = {
-        image: "https://react.semantic-ui.com/images/wireframe/image.png",
-        numbers : "not found"
-    }
-        return data.hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) =>
-        ` <a class="photo-card" href="${largeImageURL}">
-        <img src="${webformatURL || defaults.image}" alt="${tags}" width = "300" height = "200" loading="lazy" />
-        <div class="info">
-          <p class="info-item">
-            <b>Likes</b>
-            <span class = "info-item-span">${likes || defaults.numbers}</span>
-          </p>
-          <p class="info-item">
-            <b>Views</b>
-            <span class = "info-item-span">${views || defaults.numbers}</span>
-          </p>
-          <p class="info-item">
-            <b>Comments</b>
-            <span class = "info-item-span">${comments|| defaults.numbers}</span>
-          </p>
-          <p class="info-item">
-            <b>Downloads</b>
-            <span class = "info-item-span">${downloads || defaults.numbers}</span>
-          </p>
-        </div>
-      </a>`).join('')
+ async function searchImages(currentPage = "1") {
+  const params = new URLSearchParams({
+      key: "39125573-a5e5a696ee6b560e318b28a02",
+      q: elem.input.value.trim(),
+      image_type: "photo",
+      orientation: "horizontal",
+      safesearch: true,
+      per_page: 40,
+      page: currentPage
+  })
+    const response = await axios.get(`${BASE_URL}?${params}`)
+     return response.data;
 }
-
-
 
 elem.loadMoreBtn.addEventListener("click", handlerLoadMore);
 
-function handlerLoadMore() { 
+async function handlerLoadMore() { 
   page += 1;
-  searchImages(page)
-    .then((data) => {
-      elem.gallery.insertAdjacentHTML("beforeend", renderMarkupInfo(data));
-       new SimpleLightbox('.gallery a')
+  const response = await searchImages(page)
+   try {
+      elem.gallery.insertAdjacentHTML("beforeend", renderMarkupInfo(response));
+       lightbox.refresh()
        
-      const pages = Math.ceil(data.totalHits/data.hits.length)
+      const pages = Math.ceil(response.totalHits/40)
       if(page < pages) {
         elem.loadMoreBtn.classList.replace("is-hidden", "load-more")
        }
-      if(page >= pages) {
+      if(page === pages) {
         elem.loadMoreBtn.classList.replace("load-more", "is-hidden");
           return Notify.warning("We're sorry, but you've reached the end of search results.", {position: "center-bottom", fontSize: "25px", width: "450px", position: "center-bottom"});
          } 
-      })
-    .catch((err) => {
+      } catch(err) {
       elem.loadMoreBtn.classList.replace("load-more", "is-hidden");
-    });
-  
- 
+    };
 }
 
 
---
+++ src/js/markup.js
@@ -0,0 +1,28 @@
+export function renderMarkupInfo(data) {
+    const defaults = {
+        image: "https://react.semantic-ui.com/images/wireframe/image.png",
+        numbers : "not found"
+    }
+        return data.hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) =>
+        ` <a class="photo-card" href="${largeImageURL}">
+        <img src="${webformatURL || defaults.image}" alt="${tags}" height="300px" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>
+            <span class = "info-item-span">${likes || defaults.numbers}</span>
+          </p>
+          <p class="info-item">
+            <b>Views</b>
+            <span class = "info-item-span">${views || defaults.numbers}</span>
+          </p>
+          <p class="info-item">
+            <b>Comments</b>
+            <span class = "info-item-span">${comments|| defaults.numbers}</span>
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>
+            <span class = "info-item-span">${downloads || defaults.numbers}</span>
+          </p>
+        </div>
+      </a>`).join('')
+}
